a7c74a396095bc20da2cdeb3d7ef2f201fd6ae81
Jan64121 <dsw63084@student.dsw.edu.pl>
2026-05-05 11:00:24 +0200

    level reset & double jump

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,18 +6,19 @@ public class PlayerMotor : MonoBehaviour
 {
   
     Vector2 direction;
-    private Rigidbody2D rigidbody2D;
+    private new Rigidbody2D rigidbody2D;
     private bool canJump = true;
     public float maxSpeed = 10;
     public float stoppingForce = 5;
     public float speed = 10;
     public float jumpForce = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private int jumpCount;
+    public int maxJumpCount = 2;
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    // Update is called once per frame
     private void FixedUpdate()
     {
         rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
@@ -38,24 +40,24 @@ public class PlayerMotor : MonoBehaviour
 
         
     }
-
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
     }
-
     private void OnJump()
     {
         if (canJump)
         {
             rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            canJump = false;
+            jumpCount++;
+            if (jumpCount >= maxJumpCount)
+                canJump = false;
         }
 
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
+        jumpCount = 0;
     }
 }
